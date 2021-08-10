fa380dd9fc5d8c8d28f9b39d091889119109e759
imronxz <37077960+imronxz@users.noreply.github.com>
2021-08-10 23:29:16 +0700

    Revert "Optional Chaining(Checking property  that exist)"
    This reverts commit 8e4f28a4f1e5dfc454b65142ee4ad250556a3ac7.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,21 +4,31 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const weekdays = [
+  'mon',
+  'tue',
+  'wed',
+  'thu',
+  'fri',
+  'sat',
+  'sun',
+];
 const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
+  openingHours: {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekdays[5]]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
   },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+}
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -34,7 +44,12 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+  orderDelivery({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
@@ -48,28 +63,3 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
-// Checking if restaurant.openingHours.mon exist
-if (restaurant.openingHours && restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon.open); // output: nothing
-}
-
-// TODO With Optional Change
-console.log(restaurant.openingHours.mon?.open); // output: undefined
-console.log(restaurant.openingHours?.mon?.open); // output: undefined
-
-// Example
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-// TODO Optional Chaining 1 Loops Array
-for (const day of days) {
-  console.log(day);
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
-}
-
-// TODO Optional Chaining 2 Object Method
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist'); // Exist
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); // Not exist
-
-// Arrays
-const user = [{ name: 'Jonas', email: 'imronxz@gmail.com' }];
-console.log(user[0]?.email ?? 'User Array empty'); //exist arrays
