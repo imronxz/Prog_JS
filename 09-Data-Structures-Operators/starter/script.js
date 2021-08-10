bbbf2d45c5d5268e598c3dd11edab38cfdbe4229
imronxz <37077960+imronxz@users.noreply.github.com>
2021-08-10 23:29:16 +0700

    Revert "Object Literals ES6"
    This reverts commit 17ce197ee5be69734d7a98ad03e623c56a1eb444.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,31 +4,6 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const weekdays = [
-  'mon',
-  'tue',
-  'wed',
-  'thu',
-  'fri',
-  'sat',
-  'sun',
-];
-const openingHours = {
-  openingHours: {
-    [weekdays[3]]: {
-      open: 12,
-      close: 22,
-    },
-    [weekdays[4]]: {
-      open: 11,
-      close: 23,
-    },
-    [weekdays[5]]: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-}
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -37,14 +12,26 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  // TODO ES6 Enhanced Object Literals
-  openingHours,
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
-  order(starterIndex, mainIndex) {
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery({
+  orderDelivery: function ({
     starterIndex = 1,
     mainIndex = 0,
     time = '20:00',
@@ -55,11 +42,18 @@ const restaurant = {
     );
   },
 
-  orderPasta(ing1, ing2, ing3) {
+  orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
   },
 
-  orderPizza(mainIngredient, ...otherIngredients) {
+  orderPizza: function (mainIngredient, ...otherIngredients) {
     console.log(mainIngredient, otherIngredients);
   },
 };
+
+// TODO entries: Returns an iterable of key, value pairs for every entry in the array
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const [i, el] of menu.entries()) {
+  console.log(`${i +1}: ${el}`);
+}
+console.log(...menu.entries());
