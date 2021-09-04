6ee749a2e5aaff61722c93a1adf3da827a147377
imronxz <imronxz@gmail.com>
2021-09-05 00:32:53 +0700

    7

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -69,28 +69,16 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//! MAP Method
-/* Map ==> Calls a defined callback function on each element of an array, and returns an array that contains the results */
-const eurToUsd = 1.1;
-/*
-const movementsUSD = movements.map(function (mov) {
-  return mov * eurToUsd;
-});
-*/
-// Arrow Methods
-const movementsUsd = movements.map(mov => mov * eurToUsd);
+// Computing USERNAMES.
+const awalNamaKecil = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
 
-console.log(movements);
-console.log(movementsUsd);
-
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
-
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movements ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdraw'} ${Math.abs(
-      mov
-    )}`
-);
-console.log(movementsDescriptions);
+awalNamaKecil(accounts); // mis
+console.log(accounts);
