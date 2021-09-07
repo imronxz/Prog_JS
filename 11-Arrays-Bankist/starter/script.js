cbd7f83b8159eea26157bce9d98db83148e6e13a
imronxz <imronxz@gmail.com>
2021-09-07 10:20:01 +0700

    8

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -69,16 +69,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// Computing USERNAMES.
-const awalNamaKecil = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(name => name[0])
-      .join('');
-  });
-};
+// The Filter method
+/* filter: Returns the elements of an array that meet the condition specified in a callback function.
+ */
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements); //[200, 450, -400, 3000, -650, -130, 70, 1300]
+console.log(deposits); //[200, 450, 3000, 70, 1300]
 
-awalNamaKecil(accounts); // mis
-console.log(accounts);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor); //[200, 450, 3000, 70, 1300]
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals); //[-400, -650, -130]
