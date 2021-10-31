8448f558ada9ad3b4df903d6eccf990634b52d56
Huewayan <37077960+imronxz@users.noreply.github.com>
2021-10-31 08:22:01 -0700

    Revert "Up21"

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -14,32 +14,26 @@ const account1 = {
 
 const account2 = {
   owner: 'Jessica Davis',
-  movements: [500, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
-const account3 = {
-  owner: 'Muhammad Imron',
-  movements: [50000, 34000, -1500, -7900, -32100, -10000, 85000, -300],
-  interestRate: 1.3,
-  pin: 1234,
-};
 
-const account4 = {
+const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
-const account5 = {
+const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4, account5];
+const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -67,213 +61,16 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-//TODO Creating DOM displayMovements
-const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = '';
-  /*
-var mov: jika sort adalah benar, maka lakukan slice pada array movements dan-
-sort array secara descending (dari value paling besar ke paling kecil), jika salah maka movs menjadi movements
-*/
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdraw';
-
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-      <div class="movements__value">${mov}€</div>
-    </div>`;
-
-    // TODO Memasukkan var html ke index.html
-    /*
-    'afterbegin': Just inside the element, before its first child.
-    https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
-    */
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
-};
-//TODO CREATING ARRAY DOM with Array.from
-/*
- * METHODS I
- */
-/* labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('€', ''))
-  );
-  console.log(movementsUI);
-
-  // Methods 2 with
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-});
-
- * METHODS II
- */
-labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value')
-  );
-  console.log(movementsUI.map(el => Number(el.textContent.replace('€', ''))));
-
-  // Methods 2 with
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-});
-
-// TODO Hitung jumlah total value
-const calcDisplayBalance = function (acc) {
-  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} €`;
-};
-
-// TODO hitung summary
-const calcDisplaySummary = function (acc) {
-  const incomes = acc.movements
-    .filter(mov => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
-
-  const outcomes = acc.movements
-    .filter(mov => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
-
-  const interest = acc.movements
-    .filter(mov => mov > 0)
-    .map(deposit => (deposit * acc.interestRate) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr);
-      return int >= 1;
-    })
-    .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
-};
-
-// TODO Membuat username baru
-const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(name => name[0])
-      .join('');
-  });
-};
-
-createUsernames(accounts);
-
-const updateUI = function (acc) {
-  // Display movements
-  displayMovements(currentAccount.movements);
-
-  // Display balance
-  calcDisplayBalance(currentAccount);
-
-  // DIsplay summary
-  calcDisplaySummary(currentAccount);
-};
-
-// TODO Implementing Login Event Handler
-let currentAccount;
-btnLogin.addEventListener('click', function (e) {
-  // Prevent form from submitting
-  e.preventDefault();
-  console.log('Login');
-
-  currentAccount = accounts.find(
-    acc => acc.username === inputLoginUsername.value
-  );
-  console.log(currentAccount);
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // Display UI and welcome massages
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(' ')[0]
-    }`;
-    containerApp.style.opacity = 100;
-
-    // Menghilangkan input fields setelah login
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur(); //Membuat blur pointer login field
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
-
-// TODO Implementing Transfer BankApp
-btnTransfer.addEventListener('click', function (e) {
-  e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find(
-    acc => acc.username === inputTransferTo.value
-  );
-  inputTransferAmount.value = inputTransferTo.value = '';
-
-  if (
-    amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?.username !== currentAccount.username
-  ) {
-    currentAccount.movements.push(-amount);
-    receiverAcc.movements.push(amount);
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
-
-// TODO Deposited loan
-btnLoan.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  const amount = Number(inputLoanAmount.value);
-  // jika jmhl input deposit > 0 dan terdapat masukkan deposited yg mana lebih besar dari 10% maka jalankan callback function tersebut(movements)
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movements
-    currentAccount.movements.push(amount);
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-  inputLoanAmount.value = '';
-});
-
-// TODO Implementing Logout button
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (
-    inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    // Find Index : mencari nilai(boolean) di array, lebih kompleks dari indexOf
-    const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
-    );
-    console.log(index);
-
-    // Delete account
-    accounts.splice(index, 1);
-    // Hide UI setelah logout account
-    containerApp.style.opacity = 0;
-  }
-  // Clear field input accounts
-  inputCloseUsername.value = inputClosePin.value = '';
-});
-
-// TODO Membuat sorting nilai movements / balance
-let sorted = false;
-btnSort.addEventListener('click', function (e) {
-  e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
-  sorted = !sorted;
-});
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//*Separate callback
-const deposit = mov => mov < 0;
+/////////////////////////////////////////////////
