2a69958d6cbdd28c6131a0cb96566ada31b9539a
imronxz <imronxz@gmail.com>
2021-11-08 16:40:39 +0700

    7. DOM Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -96,111 +96,34 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-/* // TODO Selecting, Creating and Deleting Elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const header = document.querySelector('.header');
-console.log(header);
-
-// ? Node List
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-
-document.querySelector('section--1');
-
-// ? Html Collection
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
-
-// TODO Creating and Inserting elements
-//.insertAdjacentHTML();
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent =
-  'We use cookies to ensure that we give you the best experience on our website.';
-message.innerHTML =
-  'We use cookies to ensure that we give you the best experience on our website.<button class="btn btn--close-cookie">Got it!</button>';
-
-// header.prepend(message); // teks atas
-header.append(message); // teks bawah
-// header.append(message.cloneNode(true)); // double teks
-
-// header.before(message); // sebelum header
-// header.after(message); // setelah header
-
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    // message.remove();
-    message.parentElement.removeChild(message); //DOM preversing
-  });
-
-//TODO Styles, Attributes and classes
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
-
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height + 10) + 30 + 'px';
-
-document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.src);
-console.log(logo.className);
-console.log(logo.alt);
-
-logo.alt = 'Beautiful minimalist logo';
-
-// Non-standard attributes
-console.log(logo.attributes);
-console.log(logo.getAttribute('designer'));
-logo.setAttribute('company', 'Bankist');
-
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-// Data attributes
-console.log(logo.dataset.versionNumber);
-
-// Classes
-console.log(logo.classList);
-logo.classList.add('c', 'j');
-logo.classList.remove('c', 'j');
-logo.classList.toggle('c');
-logo.classList.contains('c'); // not includes
-
-// Don't use className, use classList
-logo.className = 'Imron';
- */
-
-/* // TODO Type of Event and Event Handlers
+// TODO DOM Traversing
 const h1 = document.querySelector('h1');
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
 
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading :D');
-};
+// GOing upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
 
-h1.addEventListener('mouseenter', alertH1);
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
 
-// h1.onmouseenter = function (e) {
-//   alert('onmouseenter: Great! You are reading the heading :D');
-// };
- */
-// TODO Event Propagation
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// TODO
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    (el.style.color = 'red'), (el.style.transform = 'scale(0.5)');
+  }
+});
