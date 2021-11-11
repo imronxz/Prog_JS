914ab7982ce8c5560edeebfb2921a32f8b2a3dbe
imronxz <imronxz@gmail.com>
2021-11-11 22:54:44 +0700

    14. Build Slider_ Part1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -217,7 +217,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // TODO Lazy loading img great for performance, with specific image
@@ -244,3 +244,51 @@ const imgObserver = new IntersectionObserver(loading, {
 });
 
 imgTarget.forEach(img => imgObserver.observe(img));
+
+// TODO Building a Slider Component_ Part 1
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlides = 0;
+const maxSlides = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.3) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+// maxSlide = 1,2,3,4
+// index 0 = 0 - 1 -> -1 * 100 = -100
+// index 1 = 1 - 1 -> 0 * 100 = 0
+// index 2 = 2 - 1 -> 1 * 100 = 100
+// index 3 = 3 - 1 -> 2 * 100 = 200
+// curSlides = -100%, 0%, 100%, 200%
+
+const nextSlide = function () {
+  // jika curSlides === maxSlides maka curSlides return curSlides 0
+  if (curSlides === maxSlides - 1) {
+    curSlides = 0;
+  } else {
+    curSlides++;
+  }
+  goToSlide(curSlides);
+};
+
+const prevSlide = () => {
+  // jika curSlides === 0 maka curSlides return curSlides maxSlides - 1
+  if (curSlides === 0) {
+    curSlides = maxSlides - 1;
+  } else {
+    curSlides--;
+  }
+
+  goToSlide(curSlides);
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
