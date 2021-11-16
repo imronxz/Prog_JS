fae9a98c21af1fc60780c3640b7568cccb8136c7
imronxz <imronxz@gmail.com>
2021-11-16 15:01:41 +0700

    16. LifeCycle of DOM

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -344,3 +344,19 @@ const slider = function () {
   });
 };
 slider();
+
+// TODO LifeCycle of DOM Events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page loaded!', e);
+});
+
+/* window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
+ */
