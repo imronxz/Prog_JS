153f7c354e7b4a3bbb8116fe33e33f1e918299a8
imronxz <imronxz@gmail.com>
2021-11-18 21:13:33 +0700

    5. ES6 Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -98,44 +98,38 @@ const h1 = document.querySelector('h1');
 console.log(h1.__proto__);
 console.dir(x => x + 1);
 
-// Coding Challenge #1
+// TODO ES6 Classes
 
-/*
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+// class expression
+// const PersonCl = class{
 
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
+// }
 
-GOOD LUCK 😀
+// TODO Class declaration
+class PersonCl {
+  constructor(namaAwal, tgglLahir) {
+    this.namaAwal = namaAwal;
+    this.tgglLahir = tgglLahir;
+  }
+  // * Methods will be added to .prototype property
+  hitungUmur() {
+    console.log(2037 - this.tgglLahir);
+  }
+  haloo() {
+    console.log(`Halo ${this.namaAwal}`);
+  }
+}
+const imronxz = new PersonCl('imron', 1996);
+imronxz.hitungUmur();
+
+console.log(imronxz.__proto__ == PersonCl.prototype);
+// PersonCl.prototype.haloo = function () {
+//   console.log(`Halo ${this.namaAwal}`);
+// };
+imronxz.haloo();
+
+/* HACK  What is Classes?
+1. Classes are NOT hoisted
+2. Class are first-class citizes
+3. Classes are execute in strict mode
 */
-
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-// TODO Prototypal Inheritance
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed}km/h`);
-};
-
-Car.prototype.break = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed}km/h`);
-};
-
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
-
-bmw.accelerate();
-bmw.accelerate();
-bmw.break();
-bmw.accelerate();
-
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.break();
-mercedes.accelerate();
