ac232d3859e8650e942e90373e6a402df406dcbd
imronxz <imronxz@gmail.com>
2021-11-18 21:56:11 +0700

    8. Object Create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -197,3 +197,27 @@ class Person1 {
 const nama1 = new Person1('Imron1', 'Muhammad');
 console.log(nama1);
 Person1.hey();
+
+// TODO Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 1990;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+// TODO Using initial
+const straven = Object.create(PersonProto);
+straven.init('Straven', 1979);
+straven.calcAge();
