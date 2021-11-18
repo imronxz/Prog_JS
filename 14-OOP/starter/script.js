e1ea292c9ced61964151f57bf0fecaa9e7461345
imronxz <imronxz@gmail.com>
2021-11-18 21:44:00 +0700

    7. Static Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -169,3 +169,31 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+
+// TODO Static Methods
+// 1. Static Methods on constructor function/ classes expression
+const Person0 = function (fName, lName) {
+  this.fName = fName;
+  this.lName = lName;
+};
+const nama0 = new Person0('imron0', 'muhammad');
+console.log(nama0);
+Person0.hey = function () {
+  console.log('Hey Classes 🍅');
+  // console.log(this);
+};
+Person0.hey();
+
+// 2. Static Methods on Classes declaration
+class Person1 {
+  constructor(fName, lName) {
+    this.fName = fName;
+    this.lName = lName;
+  }
+  static hey() {
+    console.log('Hey Classes 🍕');
+  }
+}
+const nama1 = new Person1('Imron1', 'Muhammad');
+console.log(nama1);
+Person1.hey();
