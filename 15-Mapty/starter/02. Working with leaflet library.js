5ba20ea559280f5848896b5179d95275609f860d
imronxz <imronxz@gmail.com>
2021-11-21 00:40:14 +0700

    2. leaflet library map

---
+++ 15-Mapty/starter/02. Working with leaflet library.js
@@ -0,0 +1,28 @@
+'use scripts';
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      // TODO Get latitude and longitude from geolocation
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      // TODO leaflet
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
