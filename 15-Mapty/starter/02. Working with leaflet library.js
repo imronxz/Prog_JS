234bf715f7546732c8521c2ca417335005c1b570
imronxz <imronxz@gmail.com>
2021-11-21 00:42:18 +0700

    2. Leaflet Map Library

--- 15-Mapty/starter/02. Working with leaflet library.js
+++ 15-Mapty/starter/02. Working with leaflet library.js
@@ -6,18 +6,17 @@ if (navigator.geolocation)
       // TODO Get latitude and longitude from geolocation
       const { latitude, longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
       const coords = [latitude, longitude];
 
       // TODO leaflet
-      const map = L.map('map').setView(coords, 13);
+      const map = L.map('map').setView([-2.2875, 102.7146], 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
+      L.marker([-2.2875, 102.7146])
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
