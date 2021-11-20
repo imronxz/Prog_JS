6f32e7f8954bf834943e6e265af8700514343f10
imronxz <imronxz@gmail.com>
2021-11-21 01:07:31 +0700

    3. leaflet working with an Marker

--- 15-Mapty/starter/03. Displaying a map Marker.js
+++ 15-Mapty/starter/03. Displaying a map Marker.js
@@ -1,16 +1,4 @@
 'use strict';
-
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
-
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     position => {
@@ -33,6 +21,7 @@ if (navigator.geolocation)
         console.log(mapEvent);
         const { lat, lng } = mapEvent.latlng;
 
+        // TODO leaflet Marker: https://leafletjs.com/reference.html#marker
         L.marker([lat, lng])
           .addTo(map)
           .bindPopup(
@@ -44,6 +33,7 @@ if (navigator.geolocation)
               className: '.running-popup',
             })
           )
+          // https://leafletjs.com/reference.html#layer
           .setPopupContent('Pin 📍')
           .openPopup();
       });
