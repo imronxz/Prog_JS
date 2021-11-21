afd6109f363db238399932df7a28c9a5e157b345
imronxz <imronxz@gmail.com>
2021-11-21 20:58:59 +0700

    6. Workout Data_ Creating Classes

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = Date.now() + ''.slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence; // in rpm
+    this.calcPace(); // immediately call the function
+  }
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain; // in m
+    this.calcSpeed(); // immediately call the function
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([-2.2875, 102.7146], 5.2, 24, 178);
+// const cyc1 = new Cycling([-2.2875, 102.7146], 27, 95, 520);
+// console.log(run1, cyc1);
+
+///////////////////////////////////////
+// TODO APPLICATION ARCHITECTURE
 class App {
   // TODO Private property
   #map;
