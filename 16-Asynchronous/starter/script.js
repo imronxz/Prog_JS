9ef0a06ac2a4a2867bc4930d93bad2903666a0d7
imronxz <imronxz@gmail.com>
2021-11-28 23:19:19 +0700

    3. Promises and Fetch API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -22,52 +22,19 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
-// TODO Get data from API
-const getCountryAndNeighbour = country => {
-  // * XML Http Request function
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// TODO Async/Await MODERN
+const request = fetch(`https://restcountries.com/v2/name/indonesia`);
+console.log(request);
 
-    // * Callback renderCountry function
-    renderCountry(data);
-
-    // * Get neighbour country (2)
-    const [neighbour] = data.borders;
-
-    if (!neighbour) return;
-
-    // * AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
-
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
-
-      renderCountry(data2);
-    });
-  });
+const getCountryData = function (country) {
+  // * Attaches callbacks for the resolution and/or rejection of the Promise.
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(
+      response => response.json() // json is new promises
+    )
+    .then(data => renderCountry(data[0]));
 };
-// TODO Event listener
-getCountryAndNeighbour('indonesia');
-// getCountryAndNeighbour('usa');
-
-// TODO Callback HELL
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 second passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-      setTimeout(() => {
-        console.log('4 second passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+getCountryData('indonesia');
+// getCountryData('china');
+// getCountryData('japan');
